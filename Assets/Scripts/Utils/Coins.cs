7ec175e903fe404eb70c06b7ef89a9ce8bb60c0b
Lucas Lima <lucaslima85235@gmail.com>
2022-06-26 16:55:19 -0400

    UI

---
+++ Assets/Scripts/Utils/Coins.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Coins : MonoBehaviour
+{
+    [Header("UI")]
+    public TextMeshProUGUI[] texts;
+
+    private int _coins;
+    private string _coinsDataKey = "Coins";
+
+    public int TotalCoins
+    {
+        get => _coins;
+        set
+        {
+            _coins = value;
+            SaveData();
+
+            if (texts.Length > 0)
+            {
+                foreach (var item in texts)
+                {
+                    item.text = _coins.ToString();   
+                }
+            }
+        }
+    }
+
+    public static Coins Instance;
+
+    private void Awake()
+    {
+        Instance = this;
+        LoadData();
+    }
+
+    [ContextMenu("Save Data")]
+    public void SaveData()
+    {
+        PlayerPrefs.SetInt(_coinsDataKey, TotalCoins);
+        PlayerPrefs.Save();
+    }
+
+    [ContextMenu("Load Data")]
+    public void LoadData()
+    {
+        TotalCoins = PlayerPrefs.GetInt(_coinsDataKey, 5000);
+    }
+}
